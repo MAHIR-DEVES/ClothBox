c36f441074a31df68fc19ca57e1fd8e4dca2381a
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-02-25 15:56:27 +0600

    design update

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -278,7 +278,7 @@ function ProductPage() {
               </h2>
             </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 sm:gap-6">
               {initialProducts[category].map(product => (
                 <div
                   key={product.id}
